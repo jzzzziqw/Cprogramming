cfae3608a4a394f0e729fe881a05eaccfa6f711b
jzzzziqw <jzero085@naver.com>
2022-05-13 12:35:55 +0900

    new files

--- test.c
+++ test.c
@@ -7,9 +7,9 @@ int main()
     printf("실수를 입력하세요: ");
     scanf("%f", &a);
 
-    printf("%f", a);
+    printf("%f", a); //부동소수점 표기방식
     printf("\n");
-    printf("%g", a);
+    printf("%g", a);//부동소수점과 지수 표기 방식 중 간단한 형식으로 출력
     
     return 0;
 }
